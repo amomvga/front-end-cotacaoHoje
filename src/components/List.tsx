e1f8bc7b09b8373344a4e76ebb36c60d82438102
Amom Augusto <amom_augusto@hotmail.com>
2021-11-09 00:28:42 -0300

    Bugs removidos

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import axios from "axios";
-import Australia from "./icons/IconAustralia";
-import Canada from "./icons/IconCanada";
-import Europe from "./icons/IconEurope";
-import Kingdom from "./icons/IconKingdom";
-import UnitedStates from "./icons/IconUnitedStates";
+
 
 export default function List() {
   const [coin, setCoin] = useState({
@@ -51,27 +48,27 @@ export default function List() {
         <ul>
           <li>
             <a onClick={() => handlerClick("aud")}>
-              <Australia />
+              <Image src="/img/australia_flag.jpg" width={100} height={100} />
             </a>
           </li>
           <li>
             <a onClick={() => handlerClick("cad")}>
-              <Canada />
+            <Image src="/img/canada_flag.jpg" width={100} height={100} />
             </a>
           </li>
           <li>
             <a onClick={() => handlerClick("dolar")}>
-              <UnitedStates />
+            <Image src="/img/united_states_flag.jpg" width={100} height={100} />
             </a>
           </li>
           <li>
             <a onClick={() => handlerClick("euro")}>
-              <Europe />
+            <Image src="/img/europe_flag.jpg" width={100} height={100} />
             </a>
           </li>
           <li>
             <a onClick={() => handlerClick("gbp")}>
-              <Kingdom />
+            <Image src="/img/united_kingdom_flag.jpg" width={100} height={100} />
             </a>
           </li>
         </ul>
