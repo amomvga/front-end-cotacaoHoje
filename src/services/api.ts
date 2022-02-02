c1570a4f834ae34a03f64cbbd4875bedbf6a80f8
amomvga <amom_augusto@hotmail.com>
2022-02-02 19:04:08 -0300

    commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,3 @@ import axios from "axios";
 export const coinBase = axios.create({
   baseURL: `https://economia.awesomeapi.com.br`,
 });
-
-export const marketBase = axios.create({
-  baseURL: `https://www.alphavantage.co`,
-});
